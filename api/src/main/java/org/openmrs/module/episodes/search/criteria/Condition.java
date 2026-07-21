7d5b52c827891ad0f64b55a8066788eea69986ba
Rahul Ramesh <rahul.ramesh@thoughtworks.com>
2026-07-21 05:57:06 +0530

    BAH-4876 | Add. Typed Search Criteria Models With Mutual Exclusivity Guards

---
+++ api/src/main/java/org/openmrs/module/episodes/search/criteria/Condition.java
@@ -0,0 +1,42 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at https://www.bahmni.org/license/mplv2hd.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+
+package org.openmrs.module.episodes.search.criteria;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class Condition {
+
+    private String field;
+
+    @Setter(AccessLevel.NONE)
+    private FieldComparator comparator;
+
+    private String value;
+    private ConditionOperator operator;
+    private List<Condition> conditions;
+
+    public void setComparator(String comparator) {
+        this.comparator = FieldComparator.fromString(comparator);
+    }
+
+    public boolean isLeaf() {
+        return field != null && operator == null;
+    }
+
+    public boolean isGroup() {
+        return operator != null && field == null;
+    }
+}
